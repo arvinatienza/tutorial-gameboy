7720b06cca9e3a7845468e1d646a84369548bc81
Arvin Atienza <arvin.atienza@gmail.com>
2022-02-13 00:27:15 -0600

    feat: add test sprite

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "testSprites.c"
 
 void main() {
   printf("HELLO\n");
   printf("ASA\n");
   printf("NOA\n");
   printf("IRA");
+  set_sprite_data(0,3,testSprites);
+  set_sprite_tile(0,1);
+  move_sprite(0, 88, 78);
+  SHOW_SPRITES;
 }

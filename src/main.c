fa8c0866cfcb4b50d290c97fb3c18cbbdd3a4ce4
Arvin Atienza <arvin.atienza@gmail.com>
2022-02-13 01:33:04 -0600

    feat: add controls

--- src/main.c
+++ src/main.c
@@ -7,8 +7,45 @@ void main() {
   printf("ASA\n");
   printf("NOA\n");
   printf("IRA");
-  set_sprite_data(0,3,testSprites);
-  set_sprite_tile(0,1);
+
+
+  int8_t totalSprites = 3;
+  int8_t currentSpriteIndex = 1;
+  set_sprite_data(0, totalSprites, testSprites);
+  set_sprite_tile(0, currentSpriteIndex);
   move_sprite(0, 88, 78);
   SHOW_SPRITES;
+
+  while(1) {
+    switch(joypad()) {
+      case J_UP:
+        scroll_sprite(0,0,-10);
+        break;
+      case J_DOWN:
+        scroll_sprite(0,0,10);
+        break;
+      case J_LEFT:
+        scroll_sprite(0,-10,0);
+        break;
+      case J_RIGHT:
+        scroll_sprite(0,10,0);
+        break;
+      case J_A:
+        currentSpriteIndex++;
+        break;
+      case J_B:
+        currentSpriteIndex--;
+        break;
+    }
+    if (currentSpriteIndex > totalSprites - 1) {
+      currentSpriteIndex = totalSprites - 1;
+    }
+    if (currentSpriteIndex < 0) {
+      currentSpriteIndex = 0;
+    }
+    set_sprite_tile(0,currentSpriteIndex);
+
+    delay(200);
+  }
+
 }
